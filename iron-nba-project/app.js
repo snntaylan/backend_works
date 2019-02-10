f1116e4521554e3edd4484d784f6b4967100d06c
snntaylan <sinan.taylan@outlook.com>
2019-02-10 16:34:23 +0100

    done with no partilas

---
+++ iron-nba-project/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const hbs     = require('hbs');
+const app     = express();
+const path    = require('path')
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/players', (req, res, next) => {
+  res.render('players');
+});
+
+app.get('/teams', (req, res, next) => {
+  res.render('teams');
+});
+
+app.listen(3000);
